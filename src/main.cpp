1ffb2e1a1a2e8c6616cf1648d4462d9680f5e33f
Fadi Sedik <fady88@gmail.com>
2018-08-05 10:34:53 +0200

    adding smooth lane change

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 
 #include <spline.h>
 
+#define DIST(x , y) sqrt( ( ( (x) * (x) ) + ( (y) * (y) ) ) )
 
 using namespace std;
 
@@ -354,6 +355,78 @@ int main() {
 							ptsy.push_back(ref_y);
 
 						}
+
+						vector<double> next_wp0,next_wp1,next_wp2;
+
+						next_wp0 = getXY(car_s + 30 , (2 + 4 * lane) ,map_waypoints_s , map_waypoints_x , map_waypoints_y);
+						next_wp1 = getXY(car_s + 60 , (2 + 4 * lane) ,map_waypoints_s , map_waypoints_x , map_waypoints_y);
+						next_wp2 = getXY(car_s + 90 , (2 + 4 * lane) ,map_waypoints_s , map_waypoints_x , map_waypoints_y);
+
+
+						ptsx.push_back(next_wp0[0]);
+						ptsx.push_back(next_wp1[0]);
+						ptsx.push_back(next_wp2[0]);
+
+						ptsy.push_back(next_wp0[1]);
+						ptsy.push_back(next_wp1[1]);
+						ptsy.push_back(next_wp2[1]);
+
+						for(int i = 0 ; i < ptsx.size() ; i++)
+						{
+							//transform all waypoint to car coordinates frame 
+							double shift_x = ptsx[i] - ref_x;
+							double shift_y = ptsy[i] - ref_y;
+
+							ptsx[i] =  shift_x * cos(ref_yaw) + shift_y * sin(ref_yaw);
+							ptsy[i] = -shift_x * sin(ref_yaw) + shift_y * cos(ref_yaw);
+
+						}
+
+						tk::spline s;
+
+						s.set_points(ptsx,ptsy);
+
+						for(int i = 0 ; i < previous_path_x.size() ; i++)
+						{
+							//keep last unused planned points
+							next_x_vals.push_back(previous_path_x[i]);
+							next_y_vals.push_back(previous_path_y[i]);
+						}
+
+						double target_x = 30.0;
+						double target_y = s(target_x);
+
+						double target_dist = DIST(target_x , target_y);
+
+						double x_add_on = 0 ;
+
+						//plan the remaing points in the path and always send 50
+
+						for(int i = 1 ; i <= 50 - previous_path_x.size() ; i++)
+						{
+
+							double N = target_dist / (.02 * ref_vel /2.24);
+							double x_point = x_add_on + target_x / N ;
+							double y_point = s(x_point);
+
+							x_add_on = x_point;
+
+							double x_ref = x_point , y_ref = y_point;
+
+							//change back to global coordiantes
+							x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+							y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+
+							x_point += ref_x;
+							y_point += ref_y;
+
+							next_x_vals.push_back(x_point);
+							next_y_vals.push_back(y_point);
+
+
+						}
+
 
 						#endif
 
